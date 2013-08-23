3dd628f0c3521b134d95dba7a9ec9ae180fa686a
John Mumm <jtfmumm@gmail.com>
2013-08-23 18:20:47 -0400

    Cleaned up code

--- aleat-test-interface.js
+++ aleat-test-interface.js
@@ -2,10 +2,20 @@
 
 
 (function testRun() {
+
+    //
+    //  SET UP PAGE
+    //
+
+    //SET UP CALLBACKS
+
+    //Generate MIDI file
     $('#genButton').click(function() {
         populateSettings();
         submitto('generate', 'http://l01c.ouvaton.org/cgi-bin/musicpad.cgi')
     });
+
+    //Toggle (advanced) parts menu
     $('#showAdvanced').click(function() {
         if ($('#showAdvanced').val() === 'Show Advanced') {
             $('#advanced').css('display', 'block');
@@ -16,31 +26,27 @@
         }
     });
 
-
-
+    //Changes to top menu repopulates parts menu
     $('#algorithm').change(function() {
-        if ($('#algorithm').val() === 'Repetitive') {
-            //$('.scales').css('display', 'none');
-            $('.algoOpts').val('Repetitive');
-        }
-        if ($('#algorithm').val() === 'Markov') {
-            //$('.scales').css('display', 'block');
-            $('.algoOpts').val('Markov');
-        }
+        $('.algoOpts').val($('#algorithm').val());
     });
+
     $('#rootTone').change(function() {
-        var thisTone = $('#rootTone').val();
-        $('.rootToneOpts').val(thisTone);
+        $('.rootToneOpts').val($('#rootTone').val());
     });
+
     $('#style').change(function() {
         $('.styleOpts').val($('#style').val());
     });
+
     $('#scale').change(function() {
         $('.scaleOpts').val($('#scale').val());
     });
+
     $('#instrument').change(function() {
         $('.instrumentOpts').val($('#instrument').val());
     });
+
     $('#range').change(function() {
         $('.rangeOpts').val($('#range').val());
     });
@@ -48,119 +54,32 @@
     $('#parts').change(function() {
         var count = parseInt($('#parts').val());
         $('.parts').css('display', 'none');
-        var $partEls = $('.parts');
 
         for (var i = 1; i < (count + 1); i++) {
-            if (i < 10) {
-                var j = '0' + i;
-            } else {
-                j = i;
-            }
-            $('.part' + j).css('display', 'block');
+            var partNum = zeroed(i);
+            $('.part' + partNum).css('display', 'block');
         }
     });
 
+    //GENERATE PARTS MENU FROM TEMPLATES
 
-    function populateSettings() {
-        var songSubmit;
-
-        aleat.reInit(); //re-initialize aleat.options
-        aleat.tempo = $('#tempo').val();
-        aleat.duration = $('#duration').val();
-        aleat.scale = $('#scale').val();
-        aleat.rootTone = $('#rootTone').val();
-        aleat.style = $('#style').val();
-        aleat.parts = $('#parts').val();
-
-        //Populate options object
-        for (var i = 1; i < 11; i++) {
-            if (i < 10) {
-                var j = '0' + i;
-            } else {
-                j = i;
-            }
-            aleat.options.style.push($('#' + j + 'style').val());
-        }
-         for (var i = 1; i < 11; i++) {
-            if (i < 10) {
-                var j = '0' + i;
-            } else {
-                j = i;
-            }
-            aleat.options.scale.push($('#' + j + 'scale').val());
-        }
- /*        for (var i = 1; i < 11; i++) {
-            if (i < 10) {
-                var j = '0' + i;
-            } else {
-                j = i;
-            }
-            aleat.options.algo[i - 1] = $('#' + j + 'algo').val();
-        }    */
-         for (var i = 1; i < 11; i++) {
-            if (i < 10) {
-                var j = '0' + i;
-            } else {
-                j = i;
-            }
-            aleat.options.rootTone.push($('#' + j + 'rootTone').val());
-        }
-        for (var i = 1; i < 11; i++) {
-            if (i < 10) {
-                var j = '0' + i;
-            } else {
-                j = i;
-            }
-            aleat.options.instrument.push($('#' + j + 'instrument').val());
-        }
-        for (var i = 1; i < 11; i++) {
-            if (i < 10) {
-                var j = '0' + i;
-            } else {
-                j = i;
-            }
-            aleat.options.range.push($('#' + j + 'range').val());
-        }
-
-        if ($('#algorithm').val() === 'Markov') {
-            songSubmit = aleat.genSong(aleat.genMarkov);
-        } else if ($('#algorithm').val() === 'Repetitive') {
-            songSubmit = aleat.genSong(aleat.genRep);
-        } else {
-            songSubmit = aleat.genSong(aleat.genMarkov);
-        }
-        $('#inputBox').html(songSubmit);
-
-    }
-
-    function submitto(action,url)
-    {
-        document.musicpad.btnaction.value = action;
-        document.musicpad.action = url;
-        document.musicpad.submit()
-    }
-
-    //////
-    ////
-    //
-
-    //TEMPLATES
+    //Templates
     var startCode = "<li class='parts part<%= thisPart %>' id='listPart<%= thisPart %>'><label for='part<%=  thisPart %>'>Part <%= thisPart %>:</label><ul id='part<%= thisPart %>' style='list-style-type: none' class='horizontal'></ul></li>";
 
     var codeList = "<li><label for='<%= thisPart %><%= thisOpt %>'><%= thisLabel %>:</label><select id='<%= thisPart  %><%= thisOpt %>' class='<%= thisOpt %>Opts'></select></li>";
 
+    //Generate parts menu
     var addParts = function() {
-        var lastEl = 'advanced';
+        var lastEl = 'advanced',
+            numParts = 10;
 
-        for (var i = 1; i < 11; i++) {
+        for (var i = 1; i < (numParts + 1); i++) {
             var newPart = document.createElement("newPart");
             var dataObject = {
-                thisPart: null,
+                thisPart: zeroed(i),
                 thisOpt: null,
                 thisLabel: null
             };
-
-            if (i < 10) {dataObject.thisPart = '0' + i;} else {dataObject.thisPart = i;}
 
             $('#' + lastEl).append(_.template(startCode, dataObject));
             lastEl = 'listPart' + dataObject.thisPart;
@@ -171,21 +90,19 @@
                     case 'rootTone':
                         dataObject.thisLabel = 'Root Tone';
                         break;
-                   case 'instrument':
+                    case 'instrument':
                         dataObject.thisLabel = 'Instrument';
                         break;
-                   case 'style':
+                    case 'style':
                         dataObject.thisLabel = 'Style';
                         break;
-                   case 'scale':
+                    case 'scale':
                         dataObject.thisLabel = 'Scale';
                         break;
-                   case 'range':
+                    case 'range':
                         dataObject.thisLabel = 'Range';
                         break;
                 }
-
-                dataObject.thisLabel =
 
                 $('#part' + dataObject.thisPart).append(_.template(codeList, dataObject));
             }
@@ -193,11 +110,56 @@
     };
 
 
-    //alert(styleSel[0].val());
-    var opt = document.createElement("option");
+    //
+    // MAIN FUNCTIONS
+    //
 
+    //Send parameters to aleat object
+    var populateSettings = function() {
+        var songSubmit;
 
+        aleat.reInit(); //re-initialize aleat.options
+
+        //Populate aleat global settings
+        aleat.tempo = $('#tempo').val();
+        aleat.duration = $('#duration').val();
+        aleat.scale = $('#scale').val();
+        aleat.rootTone = $('#rootTone').val();
+        aleat.style = $('#style').val();
+        aleat.parts = $('#parts').val();
+
+        //Populate options object
+        for (var i = 1; i < 11; i++) {
+            var partNum = zeroed(i);
+            aleat.options.style.push($('#' + partNum + 'style').val());
+            aleat.options.scale.push($('#' + partNum + 'scale').val());
+            aleat.options.rootTone.push($('#' + partNum + 'rootTone').val());
+            aleat.options.instrument.push($('#' + partNum + 'instrument').val());
+            aleat.options.range.push($('#' + partNum + 'range').val());
+        }
+
+        if ($('#algorithm').val() === 'Markov') {
+            songSubmit = aleat.genSong(aleat.genMarkov);
+        } else if ($('#algorithm').val() === 'Repetitive') {
+            songSubmit = aleat.genSong(aleat.genRep);
+        } else {
+            songSubmit = aleat.genSong(aleat.genMarkov);
+        }
+
+        //Write songSubmit to hidden input box for submission to MusicPad
+        $('#inputBox').html(songSubmit);
+    };
+
+    //Submit hidden input box data to MusicPad
+    var submitto = function(action,url) {
+        document.musicpad.btnaction.value = action;
+        document.musicpad.action = url;
+        document.musicpad.submit()
+    };
+
+    //Populate drop-down menus
     var addOptions = function(dropName, setting) {
+        var opt = document.createElement("option");
         for (var i = 0; i < dropName.length; i++) {
             for (var entry in setting) {
                 opt = document.createElement("option");
@@ -213,46 +175,44 @@
         }
     };
 
+    //Initialize page
     var initPage = function() {
+        $('.algoOpts').val($('#algorithm').val());
+        $('.rootToneOpts').val($('#rootTone').val());
+        $('.styleOpts').val($('#style').val());
+        $('.scaleOpts').val($('#scale').val());
+        $('.instrumentOpts').val($('#instrument').val());
 
-            if ($('#algorithm').val() === 'Repetitive') {
-                //$('.scales').css('display', 'none');
-                $('.algoOpts').val('Repetitive');
-            }
-            if ($('#algorithm').val() === 'Markov') {
-                //$('.scales').css('display', 'block');
-                $('.algoOpts').val('Markov');
-            }
-
-            var thisTone = $('#rootTone').val();
-            $('.rootToneOpts').val(thisTone);
-
-            $('.styleOpts').val($('#style').val());
-
-            $('.scaleOpts').val($('#scale').val());
-
-            $('.instrumentOpts').val($('#instrument').val());
-
-            var count = parseInt($('#parts').val());
-            $('.parts').css('display', 'none');
-            var $partEls = $('.parts');
-
+        var count = parseInt($('#parts').val());
+        $('.parts').css('display', 'none');
         $('#advanced').css('display', 'none');
 
-
         for (var i = 1; i < (count + 1); i++) {
-                if (i < 10) {
-                    var j = '0' + i;
-                } else {
-                    j = i;
-                }
-                $('.part' + j).css('display', 'block');
-            }
+                var partNum = zeroed(i);
+                $('.part' + partNum).css('display', 'block');
+        }
     };
 
 
+    //
+    //  UTILITIES
+    //
+
+    var zeroed = function(num) {
+        if (num < 10) {
+            return '0' + num;
+        } else return num;
+    };
+
+
+    //
+    //  RUN SCRIPTS
+    //
+
+    //Add parts menu to page (initially hidden)
     addParts();
 
+    //Populate drop down menus
     var styleSel = $('.styleOpts'),
         scaleSel = $('.scaleOpts'),
         rangeSel = $('.rangeOpts'),
@@ -261,8 +221,6 @@
         rootSel = $('.rootToneOpts'),
         globInstSel = $('#instrument');
         globRangeSel = $('#range');
-
-    //alert(aleat.algos instanceof Array);
 
     addOptions(globInstSel, aleat.instruments);
     addOptions(globRangeSel, aleat.ranges);
@@ -273,6 +231,7 @@
     addOptions(algoSel, aleat.algos);
     addOptions(rootSel, aleat.tones);
 
+    //Initialize page
     initPage();
 
 }());
